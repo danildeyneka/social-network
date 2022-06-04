1841b00c3b560eeb1d284b055d10412861b91c86
danil deyneka <danildeyneka@gmail.com>
2022-06-05 01:54:02 +0500

    added store

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
-import state, {subscribe} from "./redux/state";
+import store from "./redux/state";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {addPost, updateNewPost} from "./redux/state";
 import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const rerenderDOM = (state) => {
+const rerenderDOM = (state) => { // или let ???
     root.render(
         <React.StrictMode>
             <BrowserRouter>
                 <App
-                    state={state}
-                    addPost={addPost}
-                    updateNewPost={updateNewPost}
+                    state={store.getState()}
+                    addPost={store.addPost}
+                    updateNewPost={store.updateNewPost}
                 />
             </BrowserRouter>
         </React.StrictMode>
     );
 }
 
-rerenderDOM(state) // исходный рендер всего приложения
+rerenderDOM(store.getState()) // исходный рендер всего приложения
 
-subscribe(rerenderDOM)
+store.subscribe(rerenderDOM)
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,54 +1,57 @@
-let rerenderDOM = () => {
-    console.log('fasfsa')
-}
-
-let state = { // изначальная БД (хардкод). группировка по страницам
-    profilePage: {
-        postData: [
-            {id: 1, message: 'Hi there', likesCount: 4},
-            {id: 2, message: 'Hi dasdqwdqdwd3dq', likesCount: 6},
-            {id: 3, message: 'Hi 23213', likesCount: 11},
-            {id: 4, message: 'Hi', likesCount: 2},
-        ],
-        newPost: 'Введите текст',
+let store = {
+    _state: { // изначальная БД (хардкод). группировка по страницам
+        profilePage: {
+            postData: [
+                {id: 1, message: 'Hi there', likesCount: 4},
+                {id: 2, message: 'Hi dasdqwdqdwd3dq', likesCount: 6},
+                {id: 3, message: 'Hi 23213', likesCount: 11},
+                {id: 4, message: 'Hi', likesCount: 2},
+            ],
+            newPost: 'Введите текст',
+        },
+        messengerPage: {
+            dialogsData: [
+                {id: 1, name: 'Саша'},
+                {id: 2, name: 'Маша'},
+                {id: 3, name: 'Ваня'},
+                {id: 4, name: 'Артур'},
+                {id: 5, name: 'Миша'},
+                {id: 6, name: 'Слава'},
+            ],
+            messagesData: [
+                {id: 1, message: 'Hello React'},
+                {id: 2, message: 'Its cool'},
+                {id: 3, message: 'adsdad'},
+                {id: 4, message: 'f3w3'},
+                {id: 5, message: 'lorem'}
+            ]
+        }
     },
-    messengerPage: {
-        dialogsData: [
-            {id: 1, name: 'Саша'},
-            {id: 2, name: 'Маша'},
-            {id: 3, name: 'Ваня'},
-            {id: 4, name: 'Артур'},
-            {id: 5, name: 'Миша'},
-            {id: 6, name: 'Слава'},
-        ],
-        messagesData: [
-            {id: 1, message: 'Hello React'},
-            {id: 2, message: 'Its cool'},
-            {id: 3, message: 'adsdad'},
-            {id: 4, message: 'f3w3'},
-            {id: 5, message: 'lorem'}
-        ]
-    }
+    getState() {
+        return this._state
+    },
+    _callSubscriber() {
+        console.log('fasfsa')
+    },
+    addPost() {
+        const newPost = {
+            id: 5, // хардкод
+            message: this._state.profilePage.newPost, // ловим сообщение из textarea, записанное в стейт
+            likesCount: 0
+        }
+        this._state.profilePage.postData.push(newPost) // добавление поста в стейт
+        this._state.profilePage.newPost = '' // очистка поля ввода после добавления поста
+        this._callSubscriber(this._state);
+    },
+    updateNewPost(newMessage) {
+        this._state.profilePage.newPost = newMessage
+        this._callSubscriber(this._state)
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer
+    },
+
 }
 
-export const addPost = () => {
-    const newPost = {
-        id: 5, // хардкод
-        message: state.profilePage.newPost, // ловим сообщение из textarea, записанное в стейт
-        likesCount: 0
-    }
-    state.profilePage.postData.push(newPost) // добавление поста в стейт
-    state.profilePage.newPost = '' // очистка поля ввода после добавления поста
-    rerenderDOM(state);
-}
-
-export const updateNewPost = (newMessage) => {
-    state.profilePage.newPost = newMessage
-    rerenderDOM(state)
-}
-
-export const subscribe = (observer) => {
-    rerenderDOM = observer
-}
-
-export default state
+export default store
+window.store = store
