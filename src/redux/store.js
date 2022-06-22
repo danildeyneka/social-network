0346f8ea8ea8f00f88c85debaf7341b42f6a62e1
danil deyneka <danildeyneka@gmail.com>
2022-06-22 13:30:11 +0500

    auth added

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import {combineReducers, legacy_createStore} from "redux";
 import profileReducer from "./profileReducer";
 import messengerReducer from "./messengerReducer";
 import usersReducer from "./usersReducer";
+import authReducer from "./authReducer";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     messengerPage: messengerReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer
 })
 
 let store = legacy_createStore(reducers)
