991ea329c4abf76fbbdde4a7b2c0ac8439e5ecea
danil deyneka <danildeyneka@gmail.com>
2022-07-06 23:58:31 +0500

    add gh-pages deploy

--- src/index.js
+++ src/index.js
@@ -3,22 +3,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-    root.render(
-        // <React.StrictMode>
-            <BrowserRouter>
-                <Provider store={store}
-                    // нужен для работы редакса и хуков
-                >
-                    <App
-                        // dispatch={store.dispatch.bind(store)} // вместо  addPost={store.addPost.bind(store)}
-                        // уже не нужен, тк стор был вынесен через общие пропсы, а потом через Provider
-                    />
-                </Provider>
-            </BrowserRouter>
-         // </React.StrictMode> // чтобы useState не вызывался 2 раза (проверка на его размонтированность)
-    )
+root.render(
+    // <React.StrictMode>
+    <HashRouter>
+        {/*basename делает роутинг относительно домена*/}
+        <Provider store={store}>
+            {/*нужен для работы редакса и хуков*/}
+            <App/>
+        </Provider>
+    </HashRouter>
+    // </React.StrictMode> // коммент чтобы useState не вызывался 2 раза (проверка на его размонтированность)
+)
